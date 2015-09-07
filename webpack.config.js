326c0e2a6ecd466cde5f5998d80ad5e55568be57
Evan Simpson <esimpson.rescue@gmail.com>
2015-09-07 00:31:15 -0700

    Add boilerplate config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry:  [
+      "webpack-dev-server/client?http://0.0.0.0:3000", // WebpackDevServer host and port
+      "webpack/hot/only-dev-server", // "only" prevents reload on syntax errors
+      "./src/scripts/index.js" // Your appʼs entry point
+    ],
+    output: {
+        path: path.join(__dirname, "dist/scripts"),
+        filename: "index.js",
+        publicPath: "/scripts/"
+    },
+    module: {
+        loaders: [
+            {
+              test: /\.js$/,
+              loaders: ["react-hot", "babel?optional=runtime"],
+              exclude: /node_modules/
+            },
+            { test: /\.css$/, loaders: ["style-loader", "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader"] }
+        ]
+    },
+    
+    postcss: [
+      require("autoprefixer-core"),
+      require("postcss-color-rebeccapurple")
+    ],
+    
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+};
